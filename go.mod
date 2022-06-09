09fc701f3fe897939fd32f0fc116f314db87bb45
Carlo Munguia <cmunguia@denimgroup.com>
2022-06-09 00:05:27 -0500

    Maize 6
    created backend card structure to connect payment transactions from stripe, using stripe package

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module maize
 go 1.18
 
 require github.com/go-chi/chi v1.5.4
+
+require github.com/stripe/stripe-go/v72 v72.113.0
---
+++ internal/card/card.go
@@ -0,0 +1,61 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusId int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return pi, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined."
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired."
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Your card's security code is incorrect."
+	case stripe.ErrorCodeIncorrectNumber:
+		msg = "Your card number is incorrect."
+	default:
+		msg = "There was an error processing your card."
+	}
+
+	return msg
+}
