c7a2bf8817c071b6a40cc0a0cd782ac25094da6d
Carlo Munguia <cmunguia@denimgroup.com>
2022-06-19 00:58:02 -0500

    Maize 24
    created handlers on api side to create customer & subscribe to plan, updated cards.go to reflect updates, updates front end with new JS payload / json data

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"maize/internal/cards"
+	"maize/internal/models"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/stripe/stripe-go/v72"
 )
 
 type stripePayload struct {
@@ -14,8 +17,14 @@ type stripePayload struct {
 	Amount        string `json:"amount"`
 	PaymentMethod string `json:"payment_method"`
 	Email         string `json:"email"`
+	CardBrand     string `json:"card_brand"`
+	ExpiryMonth   int    `json:"exp_month"`
+	ExpiryYear    int    `json:"exp_year"`
 	LastFour      string `json:"last_four"`
 	Plan          string `json:"plan"`
+	ProductID     string `json:"product_id"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
 }
 
 type jsonResponse struct {
@@ -110,12 +119,88 @@ func (app *application) CreateCustomerAndSubscribeToPlan(w http.ResponseWriter, 
 
 	app.infoLog.Println(data.Email, data.LastFour, data.Plan, data.PaymentMethod)
 
-	ok := true
-	msg := ""
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: data.Currency,
+	}
+
+	okay := true
+	var subscription *stripe.Subscription
+	txnMsg := "Transaction successful"
+
+	stripeCustomer, msg, err := card.CreateCustomer(data.PaymentMethod, data.Email)
+	if err != nil {
+		app.errorLog.Println(err)
+		okay = false
+		txnMsg = msg
+	}
+
+	if okay {
+		subscription, err = card.SubscribeToPlan(stripeCustomer, data.Plan, data.Email, data.LastFour, "")
+		if err != nil {
+			app.errorLog.Println(err)
+			okay = false
+			txnMsg = "Error subscribing customer"
+		}
+		app.infoLog.Println("sub id is", subscription.ID)
+	}
+
+	if okay {
+		productID, err := strconv.Atoi(data.ProductID)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		customerID, err := app.SaveCustomer(data.FirstName, data.LastName, data.Email)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		amount, err := strconv.Atoi(data.Amount)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		txn := models.Transaction{
+			Amount:              amount,
+			Currency:            "usd",
+			LastFour:            data.LastFour,
+			ExpiryMonth:         data.ExpiryMonth,
+			ExpiryYear:          data.ExpiryYear,
+			TransactionStatusId: 2,
+		}
+
+		txnID, err := app.SaveTransaction(txn)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		order := models.Order{
+			MaizeID:       productID,
+			TransactionID: txnID,
+			CustomerID:    customerID,
+			StatusID:      1,
+			Quantity:      1,
+			Amount:        amount,
+			CreatedAt:     time.Now(),
+			UpdatedAt:     time.Now(),
+		}
+
+		_, err = app.SaveOrder(order)
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+	}
 
 	resp := jsonResponse{
-		OK:      ok,
-		Message: msg,
+		OK:      okay,
+		Message: txnMsg,
 	}
 	out, err := json.MarshalIndent(resp, "", "   ")
 	if err != nil {
@@ -126,4 +211,34 @@ func (app *application) CreateCustomerAndSubscribeToPlan(w http.ResponseWriter, 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
 
+}
+
+func (app *application) SaveCustomer(firstName, lastName, email string) (int, error) {
+	customer := models.Customer{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+	}
+
+	id, err := app.DB.InsertCustomer(customer)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (app *application) SaveTransaction(txn models.Transaction) (int, error) {
+	id, err := app.DB.InsertTransaction(txn)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (app *application) SaveOrder(order models.Order) (int, error) {
+	id, err := app.DB.InsertOrder(order)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
