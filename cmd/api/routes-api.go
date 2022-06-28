99a203a6d439955b947821a91c85759efe67e652
Carlo Munguia <cmunguia@denimgroup.com>
2022-06-27 22:33:04 -0500

    Maize 35
    moved virtual terminal transaction to back end by creating new handler funcs & routes, updated front end to handle this as well with new transaction JS

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -29,5 +29,12 @@ func (app *application) routes() http.Handler {
 	mux.Post("/api/authenticate", app.CreateAuthToken)
 	mux.Post("/api/is-authenticated", app.CheckAuthentication)
 
+	mux.Route("/api/admin", func(mux chi.Router) {
+		mux.Use(app.Auth)
+
+		mux.Post("/virtual-terminal-succeeded", app.VirtualTerminalPaymentSucceeded)
+
+	})
+
 	return mux
 }
