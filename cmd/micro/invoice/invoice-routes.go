d9e9e78083df27cd1cd76d297f06e8042bbc9aec
Carlo Munguia <cmunguia@denimgroup.com>
2022-07-14 22:53:23 -0500

    Maize 56
    created invoice pdf microservice, with basic handlers, routes & main file,  got basic skeleton running on port 5000, brought env handler to the folder

---
+++ cmd/micro/invoice/invoice-routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/invoice/create-and-send", app.CreateAndSendInvoice)
+
+	return mux
+}
